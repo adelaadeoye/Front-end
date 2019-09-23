45e346b1ef99231829b43c28bfc767f8f67b29fc
umekow <umekolashun@gmail.com>
2019-09-23 17:53:45 -0500

    Login Form that logs username and password

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -2,20 +2,37 @@ import React from 'react'
 import {withFormik, Field, Form} from 'formik'
 import * as Yup from 'yup'
 
-const LogInForm({touched, errors}){
+const LogInForm = ({touched, errors}) =>{
     return(
         <Form>
+            
+            {touched.username && errors.username && <p className="error_message">{errors.username}</p>}
+            <Field type="text" name="username" placeholder="username" />
 
-
+             
+            {touched.password && errors.password && <p className="error_message">{errors.password}</p>}
+            <Field type="password" name="password" placeholder="password"/>
         </Form>
     )
 }
 
 const LogIn = withFormik({
     mapPropsToValues({username, password}){
+       return { 
         username: username || "", 
-        password: password || ""
+        password: password || ""}
+    }, 
+    validationSchema:Yup.object().shape({
+        email: Yup.string()
+        .required('please enter your username'), 
+
+        password: Yup.string()
+        .required('please enter your password')
+
+    }), 
+    handleSubmit(values){
+        console.log(values)
     }
-})
+})(LogInForm)
 
 export default LogIn
