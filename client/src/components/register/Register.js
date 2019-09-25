d18878fe521ca0f9af8e9a2f269a382025603bdc
aapetsi <apetsiampiah@gmail.com>
2019-09-25 10:56:44 +0000

    Complete registration page

--- client/src/components/register/Register.js
+++ client/src/components/register/Register.js
@@ -9,6 +9,16 @@ import {
 } from "@material-ui/core";
 import * as yup from "yup";
 
+let ages = [];
+for (let i = 18; i <= 60; i++) {
+  ages.push(i);
+}
+
+let weight = [];
+for (let i = 50; i <= 300; i++) {
+  weight.push(i);
+}
+
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1
@@ -89,9 +99,74 @@ const Register = ({ errors, touched, isSubmitting }) => {
               <p className={classes.header}>{errors.password}</p>
             )}
           </div>
-          {/* <button disabled={isSubmitting} type="submit">
-        Log In
-      </button> */}
+
+          <div>
+            <Field
+              className={classes.formInput}
+              type="password"
+              name="password2"
+              placeholder="Confirm Password"
+            />
+            {touched.password2 && errors.password2 && (
+              <p className={classes.header}>{errors.password2}</p>
+            )}
+          </div>
+
+          <div>
+            <Typography className={classes.header}>Gender</Typography>
+            <Field
+              className={classes.formInput}
+              name="gender"
+              component="select"
+            >
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+            </Field>
+          </div>
+
+          <div>
+            <Typography className={classes.header}>Age</Typography>
+            <Field className={classes.formInput} name="age" component="select">
+              {ages.map((age, index) => (
+                <option key={index} value={age}>
+                  {age}
+                </option>
+              ))}
+            </Field>
+          </div>
+
+          <div>
+            <Typography className={classes.header}>Weight</Typography>
+            <Field
+              className={classes.formInput}
+              name="weight"
+              component="select"
+            >
+              {weight.map((weight, index) => (
+                <option key={index} value={weight}>
+                  {weight}
+                </option>
+              ))}
+            </Field>
+          </div>
+
+          <div>
+            <Typography className={classes.header}>
+              Days spent exercising
+            </Typography>
+            <Field
+              className={classes.formInput}
+              component="select"
+              name="exercise"
+            >
+              {["0", "1-2", "3-4", "5-6", "7"].map(days => (
+                <option key={days} value={days}>
+                  {days}
+                </option>
+              ))}
+            </Field>
+          </div>
+
           <Button
             type="submit"
             variant="contained"
@@ -111,7 +186,12 @@ const FormikForm = withFormik({
     return {
       name: "",
       email: "",
-      password: ""
+      password: "",
+      password2: "",
+      exercise: 0,
+      gender: "male",
+      age: 18,
+      weight: 50
     };
   },
   validationSchema: yup.object().shape({
@@ -126,7 +206,11 @@ const FormikForm = withFormik({
     password: yup
       .string()
       .min(6, "Password must be at least 6 characters")
-      .required("Password is required")
+      .required("Password is required"),
+    password2: yup
+      .string()
+      .min(6, "Password must be at least 6 characters")
+      .required("Confirm password field is required")
   }),
   handleSubmit(values, { resetForm, setErrors, setSubmitting }) {
     setErrors({ email: "Email is already taken" });
